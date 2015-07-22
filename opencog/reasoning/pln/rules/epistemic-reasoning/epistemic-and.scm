5cd610178ecc80f511a129906b869e077cc9ad4e
sumitsourabh <sumitsourabh@users.noreply.github.com>
2015-07-22 14:17:13 +0200

    Create epistemic-and.scm

---
+++ opencog/reasoning/pln/rules/epistemic-reasoning/epistemic-and.scm
@@ -0,0 +1,28 @@
+
+(define pln-rule-epistemic-and
+  (BindLink
+   (VariableList
+    (VariableNode "$A")
+    (VariableNode "$B"))
+   (AndLink
+    (VariableNode "$A")
+    (VariableNode "$B"))
+   (ExecutionOutputLink
+    (GroundedSchemaNode "scm: pln-formula-epistemicand")
+    (ListLink
+     (VariableNode "$A")
+     (VariableNode "$B")))))
+
+(define (pln-formula-epistemic-and A B)
+  (cog-set-tv!
+   (AndLink A B)
+   (pln-formula-epistemic-and-side-effect-free A B))
+)
+
+(define (pln-formula-epistemic-and-side-effect-free A B)
+  (let 
+      ((sA (cog-stv-strength A))
+       (sB (cog-stv-strength B))
+       (cA (cog-stv-confidence A))
+       (cB (cog-stv-confidence B)))
+    (stv ( max (sA+sB-1 0)) (min cA cB))))
