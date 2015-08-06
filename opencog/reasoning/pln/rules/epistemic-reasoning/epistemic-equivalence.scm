0d935d591cc26a26693d5c256c453226b9c2622a
sumitsourabh <sumitsourabh@users.noreply.github.com>
2015-08-06 11:26:19 +0200

    Create epistemic-equivalence.scm

---
+++ opencog/reasoning/pln/rules/epistemic-reasoning/epistemic-equivalence.scm
@@ -0,0 +1,41 @@
+
+; Implication rule for simple truth values
+;
+; Given Implication A B , calculate strength of Equivalence belief A belief B
+; -----------------------------------------------------------------------------
+
+(define pln-rule-epistemic-equivalence
+    (BindLink
+        (VariableList
+            (VariableNode "$A")
+            (VariableNode "$B"))
+        (EquivalenceLink
+            (AndLink
+                (VariableNode "$A")
+                (VariableNode "$B"))
+            (VariableNode "$A"))
+        (ExecutionOutputLink
+            (GroundedSchemaNode "scm: pln-formula-simple-epistemic-implication")
+            (ListLink
+                (VariableNode "$B")
+                (EquivalenceLink
+                    (VariableNode "$A")
+                    (VariableNode "$B"))))))
+
+; -----------------------------------------------------------------------------
+; Epistemic Equivalence Formula
+; This version has no side effects and simply returns a TruthValue
+; -----------------------------------------------------------------------------
+
+(define (pln-formula-simple-epistemic-implication-side-effect-free AB)
+    (let
+        ((sA (cog-stv-strength (gar AB))
+        )
+         (cA (cog-stv-confidence (gar AB))))
+            (stv                          ; Strength
+                (*
+                    (cog-stv-strength AB)
+                    sA*0.95)
+                (+                        ; Confidence
+                    (cog-stv-confidence AB)
+                    cA))))
