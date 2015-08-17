bf0226d8ea08a9d69526848be44b33c8b3a9c11f
sumitsourabh <sumitsourabh@users.noreply.github.com>
2015-08-17 12:09:46 +0200

    Create belief-in-belief-rule-5.scm

---
+++ opencog/reasoning/pln/rules/epistemic-reasoning/belief-in-belief-rule-5.scm
@@ -0,0 +1,23 @@
+(define pln-rule-epistemic-belief-in-belief
+  (BindLink
+   (VariableList
+    (VariableNode "$A"))
+   (BeliefLink
+    (VariableNode "$A"))
+   (ExecutionOutputLink
+    (GroundedSchemaNode "scm: pln-formula-epistemic-belief-in-belief")
+    (ListLink
+     (VariableNode "$A")
+     ))))
+
+(define (pln-formula-epistemic-belief-in-belief A)
+  (cog-set-tv!
+   (BeliefLink (BeliefLink A))
+   (pln-formula-epistemic-belief-in-belief-side-effect-free A))
+)
+
+(define (pln-formula-epistemic-belief-in-belief-side-effect-free A)
+  (let 
+      ((sA (cog-stv-strength A))
+          (cA (cog-stv-confidence A))
+    (stv ( 1) (cA))))
