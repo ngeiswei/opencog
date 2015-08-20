26d95b41e313ab1c9c9a8852f325757dd68a6a33
sumitsourabh <sumitsourabh@users.noreply.github.com>
2015-08-20 14:39:25 +0200

    Update lower-belief-rule-2.scm

--- opencog/reasoning/pln/rules/epistemic-reasoning/lower-belief-rule-2.scm
+++ opencog/reasoning/pln/rules/epistemic-reasoning/lower-belief-rule-2.scm
@@ -1,3 +1,9 @@
+; This is the implementation of the complex rule 2 (Section 2.2 of the document)
+
+; Given a proposition A, if the agent believes it with a truth value s, then he believes it with any truth value lower than s
+
+; As a result of the lower truth value, the agent's confidence level increases by the scaling factor
+
 (define pln-rule-epistemic-lower-belief
   (BindLink
    (VariableList
@@ -17,7 +23,7 @@
 )
 
 (define (pln-formula-epistemic-lower-belief-side-effect-free A)
-  (let 
+  (let (scale
       ((sA (cog-stv-strength A))
           (cA (cog-stv-confidence A))
     (stv ( sA*0.95) (cA))))
